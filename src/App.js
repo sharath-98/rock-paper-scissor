bab73f87bcb786e20a76304005d21ba6ba7af353
sharath-98 <sharathchander.p@gmail.com>
2022-06-14 18:01:25 -0500

    App intialization

--- src/App.js
+++ src/App.js
@@ -3,6 +3,14 @@ import './App.css';
 
 function App() {
   const [userMove, setUserMove] = useState('scissor');
+  const [compMove, setCompMove] = useState('scissor');
+  const [userPoints, setUserPoints] = useState(0);
+  const [compPoints, setCompPoints] = useState(0);
+  const [turn, setTurn] = useState(null);
+  const [res, setRes] = useState('Lets see who wins');
+  const [gameover, setGameover] = useState('Game Over !');
+  const options = ['rock', 'paper', 'scissor']
+
   return (
     <div className="App">
     </div>
