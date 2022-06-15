0be1d42f5ef6e574e6b08d061efeb35bafd34809
sharath-98 <sharathchander.p@gmail.com>
2022-06-15 12:56:07 -0500

    firebase features

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBgT9AJtWWTrSSLDRTB6NRiM5AYjjVuAts",
+  authDomain: "rock-paper-scissor-7e6cf.firebaseapp.com",
+  projectId: "rock-paper-scissor-7e6cf",
+  storageBucket: "rock-paper-scissor-7e6cf.appspot.com",
+  messagingSenderId: "310370659484",
+  appId: "1:310370659484:web:b7abcf0769825f9a709159",
+  measurementId: "G-B1F07G5GDD"
+};
